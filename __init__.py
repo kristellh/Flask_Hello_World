e63ffc372702bf17fe3319fd507a72cf594322e3
kristellh <k.hervo@gmail.com>
2025-02-14 11:40:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return """<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>
-            <p>Pour accéder à vos exerices cliquez <a href='./contact/'>Ici</a></p>"""
+            <p>Pour accéder à la page contact <a href='./contact/'>Ici</a></p>"""
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
