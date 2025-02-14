49df6cecbf1231ef5047469f3b2123dec830b7a2
kristellh <k.hervo@gmail.com>
2025-02-14 11:52:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def hello_world():
     return """<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>
             <p>Pour accéder à la page contact <a href='./contact/'>Ici</a></p>"""
+  
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
@@ -23,8 +24,8 @@ def carre(val_user):
     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
 
 @app.route('/somme/<int:valeur1>/<int:valeur2>')
-def somme(valeur1,valeur2):
-  resultat = valeur1 + valeur2
+def somme(valeur1, valeur2):
+    sommeres = valeur1 + valeur2
     return "<h2>La somme de vos valeurs est : </h2>" + str(valeur1 + valeur2)
                                                                                                                
 if __name__ == "__main__":
