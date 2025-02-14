fb2522a30174d5653c51df51f5c4c63b39eb8d7d
kristellh <k.hervo@gmail.com>
2025-02-14 12:24:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -28,9 +28,9 @@ def carre(val_user):
 def somme(valeur1, valeur2):
     sommeres = valeur1 + valeur2
          if (sommeres % 2 == 0): 
-           "pair" 
+           print('pair')
          else: 
-           "impair"
+           print('pair')
     return "<h2>La somme de vos valeurs est : </h2>" + str(valeur1 + valeur2)
                                                                                                                
 if __name__ == "__main__":
