51463b8ce6c112d9f53300554c547c0a4bdb92aa
kristellh <k.hervo@gmail.com>
2025-02-26 10:30:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,15 +12,20 @@ def hello_world():
             <p>Pour accéder à la page contact <a href='./contact/'>Ici</a></p>
             <p>Pour accéder à la page cnam <a href='./cnam/'>Ici</a></p>
             <p>Pour accéder à la page cv <a href='./cv/'>Ici</a></p>
-            <p>Pour accéder à la page exercice liste <a href='./Liste_Base/'>Ici</a></p>"""
+            <p>Pour accéder à la page exercice liste <a href='./Liste_Base/'>Ici</a></p>
+            <p>Pour accéder à la page exercice 2 <a href='./exercice_base2/'>Ici</a></p>"""
   
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
 
-@app.route('/Liste_Base/')
+@app.route('/exercice_base1/')
 def exercicesliste():
     return render_template('1_Liste_Base.html')
+
+@app.route('/exercice_base2/')
+def exercicesliste():
+    return render_template('Exercices_HTML.html')
   
 @app.route("/cnam/")
 def MaPremierepage():
