6647141631b3d8e2cc9c2d25d9afef45d7a71b67
kristellh <k.hervo@gmail.com>
2025-02-27 15:11:02 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ def hello_world():
            <p>Pour accéder à la page maison <a href='./maison/'>ici</a></p>
            <p>Pour accéder à la page vallet <a href='./vallet/'>ici</a></p>
           <p>Pour accéder à la page chenille <a href='./chenille/'>ici</a></p> 
-          <p>Pour accéder à la page carre <a href='./carrecss/'>ici</a></p>"""
+          <p>Pour accéder à la page carre test <a href='./carrecss/'>ici</a></p>"""
   
 @app.route('/exercices/')
 def exercices():
